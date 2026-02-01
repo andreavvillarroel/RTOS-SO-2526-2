9bc62a679d447af95f71b2853d18d97edf318030
Andrea Villarroel <andrea.villarroel@correo.unimet.edu.ve>
2026-02-01 12:47:35 -0400

    implementación de InterruptHandler y rutina de servicio (ISR)
    Se ajustó SimulationClock para que InterruptHandler pudiese funcionar correctamente

--- RTOS_Simulador/src/logica/SimulationClock.java
+++ RTOS_Simulador/src/logica/SimulationClock.java
@@ -3,17 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package logica;
+
 import modelos.Process;
+
 /**
- * hilo que marca el paso del tiempo (ciclos)
+ * Hilo que marca el paso del tiempo (ciclos)
  * @author Andrea
  */
-public class SimulationClock extends Thread{
+public class SimulationClock extends Thread {
     private int cycleDurationMs; // Duración de cada ciclo en milisegundos
     private int totalCycles;      // Contador global de ciclos
     private boolean running;      // Control del hilo
+    private boolean interrupted = false;
     
-    // proceso que está actualmente en CPU 
+    // Proceso que está actualmente en CPU 
     private Process currentProcess;
 
     public SimulationClock(int durationMs) {
@@ -21,19 +24,29 @@ public class SimulationClock extends Thread{
         this.totalCycles = 0;
         this.running = false;
     }
+    
+    public void handleInterrupt() {
+        this.interrupted = true; // Activamos la bandera de interrupción
+    }
 
     @Override
     public void run() {
         running = true;
         while (running) {
             try {
-                //  El "Tick" del reloj: Esperamos la duración del ciclo
+                // 1. Manejo de Interrupciones (ISR)
+                if (interrupted) {
+                    System.out.println("\n[KERNEL] Ejecutando ISR (Rutina de Servicio de Interrupción)...");
+                    Thread.sleep(2000); // El satélite tarda 2 segundos en recuperarse
+                    System.out.println("[KERNEL] Satélite estabilizado. Reanudando planificación.\n");
+                    interrupted = false; 
+                }
+
+                // 2. El "Tick" del reloj: Esperamos la duración del ciclo
                 Thread.sleep(cycleDurationMs);
-                
-               
                 totalCycles++;
                 
-                // Lógica de ejecución
+                // 3. Lógica de ejecución del proceso actual
                 if (currentProcess != null) {
                     if (!currentProcess.isFinished()) {
                         currentProcess.executeInstruction();
@@ -49,7 +62,6 @@ public class SimulationClock extends Thread{
                     System.out.println("[Ciclo " + totalCycles + "] CPU Ociosa (Esperando proceso...)");
                 }
 
-                
             } catch (InterruptedException e) {
                 System.err.println("Reloj interrumpido");
                 running = false;
@@ -65,5 +77,4 @@ public class SimulationClock extends Thread{
     public void setCurrentProcess(Process p) {
         this.currentProcess = p;
     }
-
 }
