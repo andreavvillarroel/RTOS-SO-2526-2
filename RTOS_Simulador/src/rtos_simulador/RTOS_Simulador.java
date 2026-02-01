580774fd14130711b573b83f0ae8e89c5836c2fa
Andrea Villarroel <andrea.villarroel@correo.unimet.edu.ve>
2026-02-01 12:17:52 -0400

    test: verificación exitosa de estructuras personalizadas y modelo PCB

--- RTOS_Simulador/src/rtos_simulador/RTOS_Simulador.java
+++ RTOS_Simulador/src/rtos_simulador/RTOS_Simulador.java
@@ -4,6 +4,8 @@
  */
 package rtos_simulador;
 
+import estructuras.MyQueue;
+import modelos.Process;
 /**
  *
  * @author Dell
@@ -14,7 +16,22 @@ public class RTOS_Simulador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     // 1. Creamos la cola de listos usando TU estructura
+        MyQueue<Process> readyQueue = new MyQueue<>();
+
+        // 2. Creamos un proceso de prueba
+        Process p1 = new Process("001", "Sensor_Altitud", 10, 1, 50);
+
+        // 3. Lo metemos en la cola
+        readyQueue.enqueue(p1);
+
+        // 4. Probamos si sale correctamente
+        Process extraido = readyQueue.dequeue();
+        System.out.println("Proceso recuperado de la cola: " + extraido.getName());
+
+        if(extraido.getName().equals("Sensor_Altitud")) {
+            System.out.println("¡Tus estructuras y el modelo funcionan perfecto!");
+        }
     }
     
 }
